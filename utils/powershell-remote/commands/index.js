016ab74affed03f467683ec7553c614ca2a49115
Toiro <toiro.gah@gmail.com>
2021-06-21 10:13:21 +0900

    Terminate がエラーになるバグの修正

--- utils/powershell-remote/commands/index.js
+++ utils/powershell-remote/commands/index.js
@@ -7,7 +7,7 @@ const scriptDirectory = path.join(scriptDir(import.meta), 'ps-scripts');
 function build(commandScript, ...args) {
   const commandPath = path.join(scriptDirectory, commandScript);
 
-  const argsStr = args.map(arg => isNaN(arg) ? `"${arg}"`: arg).join(' ');
+  const argsStr = args.map(arg => isNaN(arg) ? `"${arg}"` : arg).join(' ');
 
   return `{
     param ($Session)
@@ -23,20 +23,20 @@ function build(commandScript, ...args) {
     } else {
       Invoke-Command -ScriptBlock $command -ArgumentList $comstr
     }
-  }`
+  }`;
 }
 
 // simple function
 export async function findFiles(node, path, filter) {
-  return getJSON(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build("findFiles.ps1", path, filter));
+  return getJSON(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build('findFiles.ps1', path, filter));
 }
 
 export async function getContentFromRemote(node, path, max = 100) {
-  return getStdout(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build("getContentFromRemote.ps1", path, max));
+  return getStdout(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build('getContentFromRemote.ps1', path, max));
 }
 
 export async function moveDirectory(node, sorceDir, destDir, newName = '') {
-  return getStdout(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build("moveDirectory.ps1", sorceDir, destDir, newName));
+  return getStdout(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build('moveDirectory.ps1', sorceDir, destDir, newName));
 }
 
 export async function sendFile(node, source, dest) {
@@ -55,18 +55,17 @@ export async function sendFile(node, source, dest) {
 
 // procedure
 export async function getDslsstat(node) {
-  return getJSON(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build("getDslsstat.ps1"));
+  return getJSON(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build('getDslsstat.ps1'));
 }
 
 export async function listUserFolders(node, configFileName) {
-  return getJSON(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build("listUserFolders.ps1", node.importDirectoryRoot, configFileName));
+  return getJSON(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build('listUserFolders.ps1', node.importDirectoryRoot, configFileName));
 }
 
 export async function setupInputFromSharedDirectory(node, sorceDir, workingDir, inputfileName, newName = '') {
-  return getStdout(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build("setupInputFromSharedDirectory.ps1", sorceDir, workingDir, inputfileName, newName));
+  return getStdout(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build('setupInputFromSharedDirectory.ps1', sorceDir, workingDir, inputfileName, newName));
 }
 
 export function terminateAbaqusJob(node, job) {
-  return getStdout(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build("terminateAbaqusJob.ps1", job.name, job.status.executeDirectoryPath));
+  return getStdout(node.hostname, node.winrmCredential.user, node.winrmCredential.encryptedPassword, build('terminateAbaqusJob.ps1', job.status.executeDirectoryPath, job.name));
 }
-
